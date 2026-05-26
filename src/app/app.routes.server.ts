172b5b0a04bfda55fdc56ea94167105068ac88a2
Alex <alexmorvirc@gmail.com>
2026-05-26 11:03:32 +0200

    actualizacion stack

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return ['1', '2', '3'].map((id) => ({ id }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
